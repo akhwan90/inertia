a00f1932245c226097cd8676748005fb76a18a9f
Nur Akhwan - T14 <akhwan90@gmail.com>
2025-03-10 06:10:32 +0700

    userlists

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -11,6 +12,10 @@ class UserController extends Controller
 {
     //
     public function index() {
-        return Inertia::render('Admin/Users/Index');
+        $users = User::paginate(100);
+
+        return Inertia::render('Admin/Users/Index', compact(
+            'users'
+        ));
     }
 }
--- resources/js/Pages/Admin/Users/Index.vue
+++ resources/js/Pages/Admin/Users/Index.vue
@@ -1,8 +1,3 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-</script>
-
 <template>
 
     <Head title="Dashboard" />
@@ -10,7 +5,7 @@ import { Head } from '@inertiajs/vue3';
     <AuthenticatedLayout>
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                Dashboard
+                Data User
             </h2>
         </template>
 
@@ -20,8 +15,62 @@ import { Head } from '@inertiajs/vue3';
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         Daftar User
                     </div>
+
+                    <div class="p-4">
+                        <table class="dark:text-white w-full">
+                            <thead>
+                                <tr class="bg-slate-600">
+                                    <th class="p-3 border-b-2 border-t-2">No</th>
+                                    <th class="p-3 border-b-2 border-t-2">Email</th>
+                                    <th class="p-3 border-b-2 border-t-2">Nama</th>
+                                    <th class="p-3 border-b-2 border-t-2">Level</th>
+                                    <th class="p-3 border-b-2 border-t-2">Created at</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="user in users.data" :key="user.id">
+                                    <td class="border-b-2 p-2">{{ user.id }}</td>
+                                    <td class="border-b-2 p-2">{{ user.name }}</td>
+                                    <td class="border-b-2 p-2">{{ user.email }}</td>
+                                    <td class="border-b-2 p-2">{{ user.level }}</td>
+                                    <td class="border-b-2 p-2">{{ user.created_at }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="my-4 mx-4 flex gap-2">
+                        <button v-for="link in users.links" :key="link.label" @click="fetchPage(link.url)"
+                            :disabled="!link.url" class="px-3 py-1 border rounded-md text-white"
+                            :class="{ 'bg-gray-300': link.active, 'cursor-not-allowed': !link.url }"
+                            v-html="link.label" />
+                    </div>
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<script>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+export default {
+    components: {
+        AuthenticatedLayout,
+        Head
+    },
+    props: {
+        users: Object,
+    },
+    method: {
+        fetchData(url) {
+            if (url) {
+                Inertia.visit(url); // Memuat halaman baru menggunakan Inertia
+            }
+        }
+    }
+};
+
+
+</script>
